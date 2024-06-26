d238f681b694c9f3dab6901203dc988d71f1226e
S-design <e.sisley@icloud.com>
2024-06-26 15:05:28 +0100

    Dynamic Table

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import logo from './logo.svg';
+import TodoRowItem from './components/TodoRowItem';
 import './App.css';
 
 function App() {
+
+const todos = [
+  {rowNumber: 1, rowDescription: 'Feed puppy', rowAssigned: 'User One'},
+  {rowNumber: 2, rowDescription: 'Water Plants', rowAssigned: 'User Two'},
+  {rowNumber: 3, rowDescription: 'Make Dinner', rowAssigned: 'User One'},
+]
+
   return (
     <div className='mt-5 container'>
       <div className="card">
@@ -18,16 +25,19 @@ function App() {
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <th scope='row'>1</th>
-                <td>Feed Dog</td>
-                <td>Eric</td>
-              </tr>
-              <tr>
-                <th scope='row'>2</th>
-                <td>Get Hair Cut</td>
-                <td>Eric</td>
-              </tr>
+              <TodoRowItem rowNumber={todos[0].rowNumber} 
+              rowDescription={todos[0].rowDescription} 
+              rowAssigned={todos[0].rowAssigned}/>
+             
+              <TodoRowItem rowNumber={todos[1].rowNumber} 
+              rowDescription={todos[1].rowDescription} 
+              rowAssigned={todos[1].rowAssigned}/>
+               
+              <TodoRowItem rowNumber={todos[2].rowNumber} 
+              rowDescription={todos[2].rowDescription} 
+              rowAssigned={todos[2].rowAssigned}/>
+              
+
             </tbody>
           </table>
         </div>
