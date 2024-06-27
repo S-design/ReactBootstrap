4c979de9c8443b9128a8643381ee77894f1d95af
S-design <e.sisley@icloud.com>
2024-06-27 11:44:49 +0100

    Todos added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
+import React, {useState} from 'react';
 import './App.css';
 import TodoTable from './components/TodoTable';
+import NewTodoForm from './components/NewTodoFrorm';
 
 function App() {
 
-const todos = [
+const [todos, setTodos] = useState( [
   {rowNumber: 1, rowDescription: 'Feed puppy', rowAssigned: 'User One'},
   {rowNumber: 2, rowDescription: 'Water Plants', rowAssigned: 'User Two'},
   {rowNumber: 3, rowDescription: 'Make Dinner', rowAssigned: 'User One'},
   {rowNumber: 4, rowDescription: 'Charge phone battery', rowAssigned: 'User Two'},
 ]
+)
+
+const addTodo = (description, assigned) => {
+  if (todos.length > 0) {
+    const newTodo = {
+      rowNumber: todos.length + 1, 
+      rowDescription: description,
+      rowAssigned: assigned
+    };
+  setTodos(todos => [...todos, newTodo])
+  
+  }
+}
 
   return (
     <div className='mt-5 container'>
@@ -18,7 +33,10 @@ const todos = [
         </div>
         <div className="card-body">
         <TodoTable todos={todos}/>
-        
+        <button className='btn btn-primary' onClick={addTodo}>
+          Add New todo
+          </button>
+          <NewTodoForm addTodo={addTodo}/>
         </div>
       </div>
     </div>
