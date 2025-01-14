6e539baa2c994eacbe5bab62e4b437e759db35f2
S-design <e.sisley@icloud.com>
2025-01-14 10:13:34 +0000

    b4Branch Change

--- src/components/NewTodoFrorm.js
+++ src/components/NewTodoFrorm.js
@@ -13,16 +13,6 @@ function NewTodoForm(props) {
         }
     }
 
-    // const descriptionChange = (event) => {
-    //     console.log('description', event.target.value);
-    //     setDescription(event.target.value);
-    // }
-
-    // const assignedChange = (event) => {
-    //     console.log('assigned', event.target.value);
-    //     setAssigned(event.target.value);
-    // }
-
     return (
         <div className='mt-5'>
             <form>
